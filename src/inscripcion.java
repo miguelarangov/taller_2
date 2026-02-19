d90e435d0ff8cf5c208e884e822b82a939ea5d74
miguelarangov <miarangova@unal.edu.co>
2026-02-19 17:54:57 -0500

    versión final

--- src/inscripcion.java
+++ src/inscripcion.java
@@ -2,17 +2,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class inscripcion {
-    public String Fecha;
+    private String fecha;
     private estado estado;
     private double progreso;
     private List<leccion> leccionesCompletadas;
 
-    public inscripcion (String fecha) {
-        this.Fecha = fecha;
+    public inscripcion(String fecha) {
+        this.fecha = fecha;
         this.estado = estado.ACTIVA;
         this.progreso = 0.0;
         this.leccionesCompletadas = new ArrayList<>();
-    
-        }
+    }
 
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(estado estado) {
+        this.estado = estado;
+    }
+
+    public double getProgreso() {
+        return progreso;
+    }
+
+    public void setProgreso(double progreso) {
+        this.progreso = progreso;
+    }
+
+    public List<leccion> getLeccionesCompletadas() {
+        return leccionesCompletadas;
+    }
+
+    public void agregarLeccionCompletada(leccion leccion) {
+        this.leccionesCompletadas.add(leccion);
+    }
 }
--- src/plataforma.java
+++ src/plataforma.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class plataforma {
-    
+	private String nombre;
+	private List<curso> cursos;
+	private List<usuario> usuarios;
+
+	public plataforma(String nombre) {
+		this.nombre = nombre;
+		this.cursos = new ArrayList<>();
+		this.usuarios = new ArrayList<>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<curso> getCursos() {
+		return cursos;
+	}
+
+	public void agregarCurso(curso curso) {
+		this.cursos.add(curso);
+	}
+
+	public List<usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void agregarUsuario(usuario usuario) {
+		this.usuarios.add(usuario);
+	}
 }
