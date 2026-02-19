4c5dcbbe43164e6eed7f966724621c040e880989
peroserog <peroserog@unal.edu.co>
2026-02-19 17:38:17 -0500

    usuario

--- src/usuario.java
+++ src/usuario.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class usuario {
+    private String nombre;
+    private String email;
+    private List<inscripcion> misInscripciones;
+
+    usuario(String nombre, String email){
+        this.setNombre(nombre);
+        this.setEmail(email);
+        this.misInscripciones = new ArrayList<>();
+    }
+    public void agregarInscripcion(Inscripcion insc) {
+        this.misInscripciones.add(insc);
+    }
+public class Usuario {
     
+
+    public String getNombre(){return nombre;}
+    public String getEmail(){return email;}
+
+    public void setNombre(String nombre){this.nombre = nombre;
+    }
+    public void setEmail(String email){this.email = email;}
+}
 }
